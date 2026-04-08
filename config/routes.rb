cf1d92c89678f94f65d4bfe31faa5b4527a64dd6
hamzaiftikhhar <alihamxa366@gmail.com>
2026-04-08 14:02:48 +0500

    Added Partial form

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,11 @@ Rails.application.routes.draw do
   # resources :products # This simplifies the routing for all CRUD actions for products
 
     resource :session
-    root "products#index"
-    resources :passwords, param: :token
+    root "products#index"           #This will allow me to lists all products, and this sets the root URL ("/") to point to the index action of the ProductsController.
+    resources :passwords, param: :token     #
 
     resources :products do
-      resources :subscribers, only: [ :create ]  
+      resources :subscribers, only: [ :create ]    
     #This creates nested routes like: POST /products/:product_id/subscribers  
     # Example: /products/20/subscribers
 
