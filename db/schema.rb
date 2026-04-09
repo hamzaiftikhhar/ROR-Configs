3175672f06b7388b9ff2e67c665e04eb4e625e7b
hamzaiftikhhar <alihamxa366@gmail.com>
2026-04-09 18:08:27 +0500

    Added notes functinoality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_09_073935) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_09_130732) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -53,6 +53,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_09_073935) do
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "title"
     t.datetime "updated_at", null: false
   end
 
