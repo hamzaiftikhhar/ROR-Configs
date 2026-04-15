f319800e57db50ae9321d63acb6f13fb112bc175
hamzaiftikhhar <alihamxa366@gmail.com>
2026-04-15 11:26:56 +0500

    implemented validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,28 +1,28 @@
 class Product < ApplicationRecord
-  include Notifications
-  
+  include Notifications #Notifications module is included in the Product model, which means that all the methods defined in the Notifications module will be available as instance methods on Product objects. This allows us to keep our code organized and modular, and it also makes it easier to reuse the notification functionality across different models if needed.
+
+
+
   has_many :subscribers, dependent: :destroy
   has_one_attached :featured_image
+
   has_rich_text :description
 
-  validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
-  validates :name, presence: true
+  # validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }, on: :create 
 
+  validates :inventory_count, numericality: { greater_than_or_equal_to: 0, message: "%{value} is not valid, should be a positive number" }, on: :create && :update
 
-
-
-  after_update_commit :notify_subscribers, if: :back_in_stock?
-
-  def back_in_stock?
-    inventory_count_previously_was.zero? && inventory_count.positive?
-  end
-
-  def notify_subscribers
-    subscribers.each do |subscriber|
-      ProductMailer.with(product: self, subscriber: subscriber).in_stock.deliver_later
+  validates :name, presence: true, length: { minimum: 2, maximum: 100 } 
+ 
+  validates_each :name do |record, attr, value|  #record is the instance of the model being validated, attr is the name of the attribute being validated (in this case, :name), and value is the actual value of that attribute for the current record. The block will be executed for each record being validated, allowing you to add custom validation logic and error messages as needed.
+    if value =~ /\A\d/ 
+      record.errors.add(attr, "cannot start with a number")
     end
   end
 
 
+  
 
 end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+
+  with_options if: :admin? do
+  validates :password, length: { minimum: 10 }
+  validates :email_address, presence: true
+  end
+
+  
   has_secure_password
   has_many :sessions, dependent: :destroy
+  validates :email_address, uniqueness: true
+  normalizes :email_address, with: ->(e) { e.strip.downcase }  
 
-  normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
+
